e89423dfe6ca6d9166325948f2203c7ac05b1b55
Warda <34371863+hraa@users.noreply.github.com>
2019-07-21 23:59:03 +0100

    n

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
-public class hello {
+public class Hello {
     public static void main(String[] args) {
         for(int i=0;i<6;i++){
-        System.out.println("Hello, World");
+        System.out.println("Hello World");
     }}
 
 }
